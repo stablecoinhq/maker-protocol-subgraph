eaeb1087a2aaa9371349389a36d965a29e7a2815
pad01g <101613202+pad01g@users.noreply.github.com>
2022-11-28 07:55:06 +0000

    asserts for handleEtch

--- tests/mappings/modules/vote/dsChief/handleEtch.test.ts
+++ tests/mappings/modules/vote/dsChief/handleEtch.test.ts
@@ -4,24 +4,9 @@ import { LogNote } from '../../../../../generated/DSChief/DSChief'
 import { handleEtch } from '../../../../../src/mappings/modules/vote/dsChief'
 import { tests } from '../../../../../src/mappings/modules/tests'
 import { system as systemModule } from '../../../../../src/entities'
+import { VoteLogEtch } from '../../../../../generated/schema'
 
-
-function strRadToBytes32(value: string): Bytes {
-    const byteArray = Bytes.fromBigInt(BigInt.fromString(value))
-    const filledArray = new Uint8Array(32)
-    for (let byteArrayIndex = 0; byteArrayIndex < Math.min(31, byteArray.length); byteArrayIndex++) {
-        filledArray[byteArrayIndex] = byteArray[byteArrayIndex]
-    }
-    return Bytes.fromUint8Array(filledArray.reverse())
-}
-
-test('dsChief#handleEtch process event in case user has slate', () => {
-    const addressList = [
-        "0x231d51dbeC6E3E63Ad22078C73B70fBfD1b14265",
-        "0x33828c8c03c0975669d358297ad1e7413da8bf3e",
-        "0xdafa56dde6a66dc1bb2418ee7bf9087e8daf16f3",
-        "0xf71b70a4af37a8f8ab570cb77b542560d7285aec"
-    ]
+function createEvent(addressList: string[]): LogNote {
 
     const sig = Bytes.fromHexString("0x5123e1fa")
     const guy = "0xa16081f360e3847006db660bae1c6d1b2e17ec2a"
@@ -41,6 +26,18 @@ test('dsChief#handleEtch process event in case user has slate', () => {
         tests.helpers.params.getBytes('fax', fax),
     ]))
 
+    return event;
+}
+
+test('dsChief#handleEtch process event if old log does not exist', () => {
+    const addressList = [
+        "0x231d51dbeC6E3E63Ad22078C73B70fBfD1b14265",
+        "0x33828c8c03c0975669d358297ad1e7413da8bf3e",
+        "0xdafa56dde6a66dc1bb2418ee7bf9087e8daf16f3",
+        "0xf71b70a4af37a8f8ab570cb77b542560d7285aec"
+    ]
+    const event = createEvent(addressList)
+
     // prepare system hat
     let system = systemModule.getSystemState(event)
     const hat = "0x000"
@@ -53,12 +50,42 @@ test('dsChief#handleEtch process event in case user has slate', () => {
     const voteLogId = event.transaction.hash.toHex() + '-' + event.logIndex.toString() + "-etch"
     const voteSlateId = "0x818fdbc388be4c94f5966634dc9271db7f9c75cd1ea999a0a59f0cfb96087463"
 
-    // assert VoteLogFree is added
+    // assert VoteLogEtch is added
     assert.fieldEquals('VoteLogEtch', voteLogId, 'hat', hat)
 
     // assert VoteSlate is added
     const addressListString = "[" + addressList.map<string>(a => a.toLowerCase()).join(", ") + "]"
     assert.fieldEquals('VoteSlate', voteSlateId, 'addresses', addressListString)
+
+    clearStore()
+})
+
+test('dsChief#handleEtch process event does execute even if old log exists, because it\'s handleEtch and not handleEtchAndVote', () => {
+    const addressList = [
+        "0x231d51dbeC6E3E63Ad22078C73B70fBfD1b14265",
+        "0x33828c8c03c0975669d358297ad1e7413da8bf3e",
+        "0xdafa56dde6a66dc1bb2418ee7bf9087e8daf16f3",
+        "0xf71b70a4af37a8f8ab570cb77b542560d7285aec"
+    ]
+    const event = createEvent(addressList)
+
+    // prepare system hat
+    let system = systemModule.getSystemState(event)
+    const hat = "0x000"
+    system.hat = hat
+    system.save()
+
+    // prepare old log
+    const oldVoteLogId = event.transaction.hash.toHex() + '-' + (event.logIndex.minus(BigInt.fromString("1"))).toString() + "-etch"
+    const oldVoteLog = new VoteLogEtch(oldVoteLogId)
+    oldVoteLog.save()
+
+    // run handler
+    handleEtch(event)
+
+    // assert VoteLogEtch is added
+    const voteLogId = event.transaction.hash.toHex() + '-' + event.logIndex.toString() + "-etch"
+    assert.fieldEquals('VoteLogEtch', voteLogId, 'hat', hat)
 
     clearStore()
 })
